c515e3a04dc51958a93aacd1224a7e053dfee9bb
Lakshmid24 <100596113+Lakshmid24@users.noreply.github.com>
2022-03-29 20:40:04 +0530

    Add files via upload

---
+++ assignment3 problem1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#to sum the elements in a list
+def summation(lista):
+    s=0
+    for i in range(len(lista)):
+        lista[i]=int(lista[i])
+        s=lista[i]+s
+
+    return s
+lista=[8,2,3,0,7]
+result=summation(lista)
+print(result)
+
+
+# In[ ]:
+
+
+
+
